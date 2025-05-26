58c5683f0754f295b3bf39156a081b8cbc2aae83
Liam Arzola <liam0215@gmail.com>
2025-05-26 19:12:21 -0400

    basic workload generator

--- workload/go.mod
+++ workload/go.mod
@@ -1,3 +1,13 @@
 module github.com/liam0215/anarres/workload
 
 go 1.24.3
+
+require (
+	github.com/blueprint-uservices/blueprint/runtime v0.0.0-20250525092121-5eadb0c0735e // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	go.mongodb.org/mongo-driver v1.17.3 // indirect
+	go.opentelemetry.io/otel v1.36.0 // indirect
+	go.opentelemetry.io/otel/metric v1.36.0 // indirect
+	go.opentelemetry.io/otel/trace v1.36.0 // indirect
+	golang.org/x/exp v0.0.0-20250506013437-ce4c2cf36ca6 // indirect
+)
---
+++ workload/workloadgen/workload.go
@@ -0,0 +1,56 @@
+package workloadgen
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"strconv"
+	"time"
+
+	"github.com/liam0215/anarres/workflow/frontend"
+)
+
+// The WorkloadGen interface, which the Blueprint compiler will treat as a
+// Workflow service
+type SimpleWorkload interface {
+	ImplementsSimpleWorkload(context.Context) error
+}
+
+// workloadGen implementation
+type workloadGen struct {
+	SimpleWorkload
+
+	frontend frontend.Frontend
+}
+
+var myarg = flag.Int("myarg", 12345, "help message for myarg")
+
+func NewSimpleWorkload(ctx context.Context, frontend frontend.Frontend) (SimpleWorkload, error) {
+	return &workloadGen{frontend: frontend}, nil
+}
+
+func (s *workloadGen) Run(ctx context.Context) error {
+	fmt.Printf("myarg is %v\n", *myarg)
+	err := s.frontend.Put(ctx, strconv.Itoa(*myarg), "Hello, world!")
+	if err != nil {
+		return fmt.Errorf("error putting item: %w", err)
+	}
+	ticker := time.NewTicker(1 * time.Second)
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case t := <-ticker.C:
+			fmt.Println("Tick at", t)
+			val, err := s.frontend.Get(ctx, strconv.Itoa(*myarg))
+			if err != nil {
+				return err
+			}
+			fmt.Println("Key: ", *myarg, ", Value: ", len(val))
+		}
+	}
+}
+
+func (s *workloadGen) ImplementsSimpleWorkload(context.Context) error {
+	return nil
+}
